3a7d6a3ca9c2b263f2e2754179d070d41261e287
Saafo <saffordh@163.com>
2019-12-28 13:42:40 +0800

    Chapter 10 finished

---
+++ Chapter10/LANChatClient.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class LANChatClient {
+    JTextArea ReceivedMessageArea;
+    JTextField MessageToBeSentField;
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket sock;
+    String LastSentMessage;
+
+    public static void main(String[] args) {
+        LANChatClient client = new LANChatClient();
+        client.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame("LAN Chat Client");
+        JPanel mainPanel = new JPanel();
+        ReceivedMessageArea = new JTextArea(15,30);
+        ReceivedMessageArea.setLineWrap(true);
+        ReceivedMessageArea.setWrapStyleWord(true);
+        ReceivedMessageArea.setEditable(false);
+        JScrollPane scroller = new JScrollPane(ReceivedMessageArea);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        MessageToBeSentField = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(scroller);
+        mainPanel.add(MessageToBeSentField);
+        mainPanel.add(sendButton);
+        setUpNetworking();
+
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(400, 310);
+        frame.setVisible(true);
+    }//关闭go
+
+    private void setUpNetworking() {
+        try {
+            sock = new Socket("127.0.0.1",4999);
+            InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+            reader = new BufferedReader(streamReader);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("networking established");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public class SendButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            try {
+                writer.println(MessageToBeSentField.getText());
+                writer.flush();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            ReceivedMessageArea.append("sent: " + MessageToBeSentField.getText() + "\n");
+            LastSentMessage = MessageToBeSentField.getText();
+            MessageToBeSentField.setText("");
+            MessageToBeSentField.requestFocus();
+        }
+    }
+
+    public class IncomingReader implements Runnable {
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    if(!message.equals(LastSentMessage)) {
+                        System.out.println("recv: " + message);
+                        ReceivedMessageArea.append("recv: " + message + "\n");
+                    }
+                }
+            } catch (Exception ex) {ex.printStackTrace();}
+        }//close run
+    }//close inner class
+}
---
+++ Chapter10/LANChatServer.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class LANChatServer {
+    ArrayList clientOutputStreams;
+
+    public class ClientHandler implements Runnable {
+        BufferedReader reader;
+        Socket sock;
+
+        public ClientHandler(Socket clientSocket) {
+            try {
+                sock = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(isReader);
+            } catch(Exception ex) {ex.printStackTrace();}
+        }
+
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("recv: " + message);
+                    tellEveryone(message);
+                }
+            } catch(Exception ex) {ex.printStackTrace();}
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new LANChatServer().go();
+    }
+
+    public void go() {
+        clientOutputStreams = new ArrayList<>();
+        try {
+            ServerSocket serverSock = new ServerSocket(4999);
+
+            while(true) {
+                Socket clientSocket = serverSock.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutputStreams.add(writer);
+
+                Thread t = new Thread(new ClientHandler(clientSocket));
+                t.start();
+                System.out.println("got a connection");
+            }
+        } catch (Exception ex) {ex.printStackTrace();}
+    }
+
+    public void tellEveryone(String message) {
+        Iterator it = clientOutputStreams.iterator();
+        while(it.hasNext()) {
+            try {
+                PrintWriter writer = (PrintWriter) it.next();
+                writer.println(message);;
+                writer.flush();
+            } catch(Exception ex){ex.printStackTrace();}
+        }
+    }
+}
