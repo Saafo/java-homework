ad50a22cce9553ddc7c7cf8ce5a38d3f53f9f8af
Saafo <saffordh@163.com>
2019-12-30 10:11:21 +0800

    update Chapter8 with quadra-threads merge sort

--- Chapter8/QuadMerge.java
+++ Chapter8/QuadMerge.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
 
-public class DualMerge extends Thread{
+public class QuadMerge extends Thread{
 
     private static void Merge(int[] l,int[] r,int[] res){
         int i=0;
@@ -41,15 +41,19 @@ public class DualMerge extends Thread{
     }
 
     private static void multiMergeSort(int a[]){
-        if(a.length>1){
-            int[] l = Arrays.copyOfRange(a,0,a.length/2);
-            int[] r = Arrays.copyOfRange(a,a.length/2,a.length);
-            CountDownLatch latch = new CountDownLatch(2);
+        if(a.length>3){
+            int[] m1 = Arrays.copyOfRange(a,0,a.length/4);
+            int[] m2 = Arrays.copyOfRange(a,a.length/4,a.length/2);
+            int[] m3 = Arrays.copyOfRange(a,a.length/2,a.length*3/4);
+            int[] m4 = Arrays.copyOfRange(a,a.length*3/4,a.length);
+            int[] a1 = new int[a.length/2];
+            int[] a2 = new int[a.length/2];
+            CountDownLatch latch = new CountDownLatch(4);
 
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    normalMergeSort(l);
+                    normalMergeSort(m1);
                     latch.countDown();
                 }
             }).start();
@@ -57,10 +61,27 @@ public class DualMerge extends Thread{
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    normalMergeSort(r);
+                    normalMergeSort(m2);
                     latch.countDown();
                 }
             }).start();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    normalMergeSort(m3);
+                    latch.countDown();
+                }
+            }).start();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    normalMergeSort(m4);
+                    latch.countDown();
+                }
+            }).start();
+
 
             try{
                 latch.await();
@@ -68,7 +89,30 @@ public class DualMerge extends Thread{
                 ex.printStackTrace();
             }
 
-            Merge(l,r,a);
+            CountDownLatch latch2 = new CountDownLatch(2);
+            new Thread(new Runnable(){
+            
+                @Override
+                public void run() {
+                    Merge(m1,m2,a1);
+                    latch2.countDown();
+                }
+            }).start();
+            new Thread(new Runnable(){
+            
+                @Override
+                public void run() {
+                    Merge(m3,m4,a2);
+                    latch2.countDown();
+                }
+            }).start();
+            try{
+                latch2.await();
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+
+            Merge(a1,a2,a);
         }
     }
 
@@ -90,7 +134,10 @@ public class DualMerge extends Thread{
         long time3 = System.currentTimeMillis();
         multiMergeSort(c);
         long time4 = System.currentTimeMillis();
-        System.out.println("The cost of the dual-threads merge sort is "+(time4-time3));
+        System.out.println("The cost of the quadra-threads merge sort is "+(time4-time3));
+        // for(int item : c) {
+        //     System.out.print(item+" ");
+        // }
 
     }
 
